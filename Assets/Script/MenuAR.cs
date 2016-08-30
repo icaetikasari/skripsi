4b1955868dce0781123e1bb072ec30a4a562cbce
unknown <icea.upz@gmail.com>
2016-08-30 16:41:48 +0700

    coba lagi nih

---
+++ Assets/Script/MenuAR.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAR : MonoBehaviour {
+
+	//variable resize layar
+	public GUISkin guiSkin;
+	private float guiRatio;
+	private float sWidth;
+	private Vector3 GuisF;
+
+	public GameObject askripsi;
+	public float kecepatanRotasi = 10f;
+	bool statusRotasi = false;
+
+	void awake(){
+		sWidth = Screen.width;
+		guiRatio = sWidth / 1920;
+		GuisF = new Vector3 (guiRatio,guiRatio, 1);
+	}
+
+	void onGUI(){
+		GUI.skin = guiSkin;
+	}
+
+
+	void Rotasi(){
+		GUI.matrix = Matrix4x4.TRS (new Vector3 (Screen.width-258*GuisF.x,GuisF.y, 0),Quaternion.identity,GuisF);
+
+		if (statusRotasi==false){
+			if(GUI.Button(new Rect(-208,10,238,59),"Rotasi")){
+				statusRotasi = true;
+			}
+		}else{
+			if (GUI.Button (new Rect (-208, 10, 238, 59), "Stop Rotasi")) {
+				statusRotasi = false;
+			}
+		}
+		if(GUI.Button(new Rect(40,10,208,59),"Keluar")){
+			Application.Quit();
+		}
+	}
+
+
+}
---
+++ Assets/Script/MenuLogic.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuLogic : MonoBehaviour {
+	public GameObject menuARPanel;
+	public GameObject virtualisasiPanel;
+	public GameObject bantuanPanel;
+	public GameObject aboutPanel;
+	public GameObject fasilitasPanel;
+	public GameObject infoPanel;
+	public GameObject panggungPanel;
+	public GameObject tengahPanel;
+	public GameObject depanPanel;
+	public LoadSceneMode loadMode = LoadSceneMode.Single;
+
+
+	public void LoadScene(string sceneName){
+		SceneManager.LoadScene (sceneName, loadMode);	
+	}
+
+	// Use this for initialization
+	void Start () {
+		Screen.autorotateToLandscapeRight = true;
+		menuARPanel.SetActive (true);
+		bantuanPanel.SetActive (false);
+		aboutPanel.SetActive (false);
+	}
+
+	public void BantuanClicked(){
+		bantuanPanel.SetActive (true);
+		menuARPanel.SetActive (false);
+		aboutPanel.SetActive (false);
+	}
+
+	public void AboutClicked(){
+		aboutPanel.SetActive (true);
+		menuARPanel.SetActive (false);
+		bantuanPanel.SetActive (false);
+	}
+
+	public void QuitClicked(){
+		Application.Quit ();
+	}
+
+	public void FasilitasClicked(){
+		fasilitasPanel.SetActive (true);
+		infoPanel.SetActive (false);
+	}
+
+	public void TengahClicked(){
+		tengahPanel.SetActive (true);
+		panggungPanel.SetActive (false);
+		depanPanel.SetActive (false);
+	}
+
+	public void PanggungClicked(){
+		panggungPanel.SetActive (true);
+		tengahPanel.SetActive (false);
+		depanPanel.SetActive (false);
+	}
+
+	public void DepanClicked(){
+		depanPanel.SetActive (true);
+		panggungPanel.SetActive (false);
+		tengahPanel.SetActive (false);
+	}
+}
